b8ebc998026427bcbf9f74ec011e2fb0dc1422ab
rukyaislam <72370986+rukyaislam@users.noreply.github.com>
2020-11-29 12:04:40 +0600

    Add files via upload

---
+++ process.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gulistan Seba</title>
+    <link rel="stylesheet"href="/css/bootstrap.min.css">
+    <link rel="stylesheet"href="/css/ bootstrap.min.js"> 
+   
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet"href="style.css">
+    <style>
+      div.scrollmenu {
+        background-color: #fff;
+        overflow: auto;
+        white-space: nowrap;
+      }
+      
+      div.scrollmenu a {
+        display: inline-block;
+        color: black;
+        text-align: center;
+        padding: 14px;
+        text-decoration: none;
+      }
+      
+      div.scrollmenu a:hover {
+        background-color:blueviolet;
+      }
+      </style>
+    
+      
+
+
+
+
+
+  </head>
+
+  
+<body>
+    <br>
+    <br>
+    <br>
+    
+  
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 style="margin-left: 10%;">Repair your phone at doorstep</h1>
+      </div>
+   
+      
+        <div class="col">
+            <img src="Screenshot_1.png" alt="Flowers in Chania"> 
+        </div>
+      </div>
+    </div> 
+
+    <div class="container"  style="background-color: white;">
+        <div class="row">
+          <div class="col"  class="w3-hover-opacity">
+            
+            <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile"><img src="repair-icon1.png " alt="Flowers in Chania" ></a>
+            
+            
+            
+            <br>
+            <strong> Schedule Service</strong>
+
+<p>Book a free technician visit at your home or work at a time slot that best suits your convenience.</p>
+          </div>
+       
+          <div class="col">
+            <a href="https://www.w3schools.com"><img src="repair-icon2.png " alt="Flowers in Chania"></a>  
+                
+             <br>
+            <strong>Check Price</strong>
+
+              <p>Tell us which phone has to be repaired. Get the best pricing.</p>
+          </div>
+          <div class="col">
+            <a href="https://www.w3schools.com"> <img src="repair-icon3.png " alt="Flowers in Chania"> </a><br>
+            <strong>Expert Technician</strong> 
+
+<p>Our super-skilled technician will be there and make it as good as new.</p></div>
+          </div>
+        </div> 
+    </div> <br> <br> <br> <br>
+<p style="margin-left: 10%;margin-right: 10%;"><b>We fix your mobile at your convenience
+</b>
+  Gulistan seba  is a revolutionary service designed to simplify mobile repairs without making them too hard on your wallet. Find the mobile that needs to get repaired, select its colour, and that's it. You will be given our range of services to choose from, like - mobile screen, mic, battery, speaker, receiver, charging jack replacement with the best possible quote for your mobile phone repair.
+  Technicians will come to your doorstep to work on the device and repair it. Cashify Repair even offers a 6-month warranty and a week-long money back guarantee. Can it get any better than this?</p>
+
+
+  <br>
+  <br>
+  <br> 
+  <p style="margin-left: 30%;margin-top: 12%;color: blueviolet;font-size: 50px;">Services Available</>
+    <br>
+    <br>
+    <br>
+    <div class="scrollmenu">
+      <a href="#home"><img src="mic.png" alt="Flowers in Chania">microphn </a>
+      <a href="#news"><img src="promi.png" alt="Flowers in Chania">PROXIMITY SENSORPROXIMITY SENSOR
+       </a>
+      <a href="#contact"><img src="jack.png" alt="Flowers in Chania">AUX JACKAUX JACK</a>
+      <a href="#about"><img src="recever.png" alt="Flowers in Chania">RECEIVER</a>
+      <a href="#support"><img src="speaker.png" alt="Flowers in Chania">SPEAKERSPEAKER</a>
+      <a href="#blog"><img src="battery.png" alt="Flowers in Chania">
+        BATTERY</a>
+      <a href="#tools"><img src="charging.png" alt="Flowers in Chania"> CHARGING JACKCHARGING JACK</a>  
+      <a href="#tools"><img src="back.png" alt="Flowers in Chania">BACK PANELBACK PANEL</a> 
+      <a href="#base"><img src="Mask_Group_135_f84f5361531b.png" alt="Flowers in Chania">SCREEN</a>
+     
+    </div>
+
+<br>
+<br><br><br>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>  
+</body>
+</html>
